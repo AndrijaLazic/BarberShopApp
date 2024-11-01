9469d9d2f3567b20d57fb9adf37e0c8935dd0cb3
AndrijaLazic <lazic.andrija223@gmail.com>
2024-11-01 18:16:18 +0100

    Added configuration server

--- AdminMicroservice/src/main/java/com/BarberShop/AdminMicroservice/AdminMicroserviceApplication.java
+++ AdminMicroservice/src/main/java/com/BarberShop/AdminMicroservice/AdminMicroserviceApplication.java
@@ -1,13 +1,13 @@
 package com.BarberShop.AdminMicroservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class AdminMicroserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AdminMicroserviceApplication.class, args);
 	}
-
 }
