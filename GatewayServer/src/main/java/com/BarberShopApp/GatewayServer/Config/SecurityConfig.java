1e4d86026fba5bf23f6a323c56b0b08fac5ec8f8
AndrijaLazic <lazic.andrija223@gmail.com>
2024-11-23 23:57:41 +0100

    Implemented roles for authentication

--- GatewayServer/src/main/java/com/BarberShopApp/GatewayServer/Config/SecurityConfig.java
+++ GatewayServer/src/main/java/com/BarberShopApp/GatewayServer/Config/SecurityConfig.java
@@ -2,13 +2,19 @@ package com.BarberShopApp.GatewayServer.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,12 +41,12 @@ public class SecurityConfig {
                 exchange.pathMatchers("/admin"+freeResourceUrls[i]).permitAll();
             }
             exchange.pathMatchers("/user/actuator/**").permitAll();
-            exchange.pathMatchers("/user/**").authenticated();
+            exchange.pathMatchers("/user/**").hasAnyRole("app_user","app_admin");
             exchange.pathMatchers("/admin/Auth/login").permitAll();
             exchange.pathMatchers("/admin/actuator/**").permitAll();
-            exchange.pathMatchers("/admin/**").authenticated();
+            exchange.pathMatchers("/admin/**").hasRole("app_admin");
             exchange.pathMatchers("/**").permitAll();
-        }).oauth2ResourceServer(rsc->rsc.jwt(Customizer.withDefaults()));
+        }).oauth2ResourceServer(rsc->rsc.jwt(jwt->jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor())));
 
         http.csrf(csrfSpec -> csrfSpec.disable());
         return http.build();
@@ -59,5 +65,13 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", corsConfig);
 
         return new CorsWebFilter(source);
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
