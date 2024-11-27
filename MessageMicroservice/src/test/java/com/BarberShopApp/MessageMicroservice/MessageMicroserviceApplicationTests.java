cf82d1db6223f3fcf69633a7a7c03084b5db2c81
AndrijaLazic <lazic.andrija223@gmail.com>
2024-11-27 18:12:21 +0100

    Started working on message microservice

---
+++ MessageMicroservice/src/test/java/com/BarberShopApp/MessageMicroservice/MessageMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarberShopApp.MessageMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
