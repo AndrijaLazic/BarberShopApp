06ec3550457213cac5adfc8ecc4605c38fe670ee
AndrijaLazic <lazic.andrija223@gmail.com>
2024-12-05 17:21:24 +0100

    Created kafka message service

---
+++ KafkaMessageService/src/main/java/com/BarberShopApp/KafkaMessageService/KafkaMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.BarberShopApp.KafkaMessageService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessageServiceApplication.class, args);
+	}
+
+}
---
+++ KafkaMessageService/src/test/java/com/BarberShopApp/KafkaMessageService/KafkaMessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BarberShopApp.KafkaMessageService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
