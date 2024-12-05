4b74d729504e5036b66fde77d246b6ec7867391a
AndrijaLazic <lazic.andrija223@gmail.com>
2024-12-05 17:21:53 +0100

    Created connection from Admin service to kafka

---
+++ AdminMicroservice/src/main/java/com/BarberShop/AdminMicroservice/DTOS/AccountsMsgDto.java
@@ -0,0 +1,5 @@
+package com.BarberShop.AdminMicroservice.DTOS;
+
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
+
+}
---
+++ KafkaMessageService/src/main/java/com/BarberShopApp/KafkaMessageService/DTO/AccountsMsgDto.java
@@ -0,0 +1,5 @@
+package com.BarberShopApp.KafkaMessageService.DTO;
+
+public record AccountsMsgDto(Long accountNumber, String name, String email, String mobileNumber) {
+
+}
